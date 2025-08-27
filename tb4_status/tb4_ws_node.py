6f7a07bf87d17c677e2a8ebf8b8bdc2fa1fb2508
bmanara <bmacraze@gmail.com>
2025-08-27 16:35:46 +0800

    Add speed tracking

--- tb4_status/tb4_ws_node.py
+++ tb4_status/tb4_ws_node.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import math
 import threading
 import websockets
 
@@ -9,6 +10,7 @@ from rclpy.qos import qos_profile_sensor_data
 
 from sensor_msgs.msg import BatteryState
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
 
 
 class TB4WsNode(Node):
@@ -16,6 +18,7 @@ class TB4WsNode(Node):
     TurtleBot 4 WebSocket Node
     This node provides a WebSocket server to receive updates from the TurtleBot 4 robot.
     It allows clients to connect and receive real-time updates on the robot's status.
+    This node will also keep track of the robot's status.
     """
     def __init__(self):
         super().__init__('tb4_ws_node')
@@ -23,11 +26,15 @@ class TB4WsNode(Node):
         self.uri = f"ws://10.100.111.76:5100/ws"
         self.websocket = None
         self.loop = None
+        self.status = 'ready'
+        self.latest_odom_msg = None
 
         # State of robot
         self.battery_percent = None
-        self.current_location_x = 0.0 
+        self.current_location_x = 0.0
         self.current_location_y = 0.0
+
+        self.map_name = 'level5_arena_v3'
 
         # Subscribers
         self.battery_state_subscriber = self.create_subscription(
@@ -44,41 +51,69 @@ class TB4WsNode(Node):
             10
         )
 
+        self.odom_subscriber = self.create_subscription(
+            Odometry,
+            'odom',
+            self.odom_callback,
+            qos_profile_sensor_data
+        )
+
     def battery_state_callback(self, msg: BatteryState):
         """
         Callback for battery state updates.
-        This method is called whenever a new battery state message is received.
-        It can be used to send updates to connected WebSocket clients.
         """
         self.battery_percent = msg.percentage * 100 if msg.percentage is not None else 0.0
 
     def amcl_pose_callback(self, msg: PoseWithCovarianceStamped):
         """
         Callback for AMCL pose updates.
-        This method is called whenever a new AMCL pose message is received.
-        It can be used to send updates to connected WebSocket clients.
         """
         self.current_location_x = msg.pose.pose.position.x
         self.current_location_y = msg.pose.pose.position.y
 
+    def odom_callback(self, msg: Odometry):
+        """
+        Callback for odometry updates.
+        """
+        self.latest_odom_msg = msg
+
+    def calculate_speed(self):
+        """
+        Calculate the current speed of the robot from the latest odometry message.
+        """
+        if not self.latest_odom_msg:
+            return 0.0
+
+        linear_velocity = self.latest_odom_msg.twist.twist.linear
+        vx = linear_velocity.x
+        vy = linear_velocity.y
+        vz = linear_velocity.z
+        linear_speed = math.sqrt(vx**2 + vy**2 + vz**2)
+
+        return linear_speed
+
     async def status_update_loop(self):
+        """
+        Periodically send status updates to connected WebSocket clients.
+        """
         while rclpy.ok():
             current_location = {
                 'x': self.current_location_x,
                 'y': self.current_location_y
             }
-            
+
+            curr_speed = self.calculate_speed()
             payload = {
                 'auc_id': self.get_namespace().strip('/'),
                 'battery_percent': self.battery_percent,
                 'ip_address': self.ip_address,
                 'location': current_location,
-                'status': 1,
+                'status': self.status,
                 'task_id': 0,
                 'timestamp': self.get_clock().now().to_msg().sec,
                 'waypoints': None,
-                'curr_speed': 2.0,
-                'map_name': 'default_map',
+                'curr_speed': curr_speed,
+                'map_name': self.map_name,
                 'speed_limit': 5.0
             }
 
@@ -96,7 +131,6 @@ class TB4WsNode(Node):
     def schedule_websocket_message(self, data):
         if self.loop and not self.loop.is_closed():
             asyncio.run_coroutine_threadsafe(self.send_websocket_message(data), self.loop)
-
 
     async def send_websocket_message(self, data):
         if self.websocket: 
